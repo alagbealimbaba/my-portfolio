824abb27a6f5e1308cfb740a9473960e63774050
Alagbe Alim <your.alagbealim81@gmail.com>
2023-08-21 10:32:45 +0100

    added media links...

--- src/components/Banner/Media.jsx
+++ src/components/Banner/Media.jsx
@@ -5,21 +5,37 @@ const Media = () => {
   return (
     <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between ">
         <div>
-          <h2 className="text-base uppercase font-titleFont mb-4">
-            Message me on
-          </h2>
-          <div className="flex gap-4 ">
-            <span className="bannerIcon">
-              <FaWhatsapp />
-            </span>
-            <span className="bannerIcon">
-              <FaInstagram />
-            </span>
-            <span className="bannerIcon">
-              <FaLinkedinIn />
-            </span>
-          </div>
-        </div>
+  <h2 className="text-base uppercase font-titleFont mb-4">
+    Message me on
+  </h2>
+  <div className="flex gap-4">
+    <a
+      href="https://wa.me/+2347069935849"  // Replace with your WhatsApp link
+      className="bannerIcon"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FaWhatsapp />
+    </a>
+    <a
+      href="https://www.instagram.com/aitzcaptain/"  // Replace with your Instagram link
+      className="bannerIcon"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FaInstagram />
+    </a>
+    <a
+      href="www.linkedin.com/in/alagbe-alim-5b5a9824b"  // Replace with your LinkedIn link
+      className="bannerIcon"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FaLinkedinIn />
+    </a>
+  </div>
+</div>
+
         <div>
           <h2 className="text-base uppercase font-titleFont mb-4">
             BEST SKILL ON
